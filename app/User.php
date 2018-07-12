3988c0bc6e5f6aca123a2f2f755b4516a026ae3f
Jerrik <jerrik@sendlane.com>
2018-07-11 23:26:45 -0700

    User and add users migrations

--- app/User.php
+++ app/User.php
@@ -10,6 +10,12 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
@@ -26,4 +32,35 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+   * Get the unique identifier for the user.
+   *
+   * @return mixed
+   */
+  public function getAuthIdentifier()
+  {
+    return $this->getKey();
+  }
+
+  /**
+   * Get the password for the user.
+   *
+   * @return string
+   */
+  public function getAuthPassword()
+  {
+    return $this->password;
+  }
+
+  /**
+   * Get the e-mail address where password reminders are sent.
+   *
+   * @return string
+   */
+  public function getReminderEmail()
+  {
+    return $this->email;
+  }
+
 }
