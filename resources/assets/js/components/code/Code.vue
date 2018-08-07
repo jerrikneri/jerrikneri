cc3208dbd2993519596221bb3b8d5c7234713ae5
Jerrik <jerrik.neri@gmail.com>
2018-08-07 06:37:02 -0700

    Blog & Project content

--- resources/assets/js/components/code/Code.vue
+++ resources/assets/js/components/code/Code.vue
@@ -9,6 +9,29 @@
     <section v-show="preview == null" class="box content has-text-centered is-large">
       <span class="title is-size-2"><u>PROJECTS</u></span>
       <div class="section">
+        <h3>JERRIKNERI.COM</h3>
+        <ul class="text-center">
+          <li>Laravel</li>
+          <li>VueJS</li>
+          <li>SASS & Bulma</li>
+        </ul>
+      </div>
+      <div class="section">
+        <h3>Hackintosh</h3>
+        <ul class="text-center">
+          <li>Intel i7 8700k</li>
+          <li>Noctua D15S</li>
+          <li>ASUS ROG STRIX Z370-G GAMING</li>
+          <li>Crucial Ballistix Sport LT 64 GB</li>
+          <li>Sapphire RX 580 Pulse 8GB</li>
+          <li>Samsung 850 EVO 1TB</li>
+          <li>EVGA G2 850W Fully Modular</li>
+          <li>Lian Li PC-A05FNA Case</li>
+          <li>TP-Link Archer T9E 802.11AC AC1900</li>
+        </ul>
+      </div>
+      
+      <div class="section">
         <h3>Let's Split</h3>
         <ul class="text-center">
           <li>Acrylic sandwich plate</li>
