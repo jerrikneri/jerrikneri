c4df5ae81af928515631bcc5fd1a6e844d46ba70
Jerrik <jerrik+intelmac@sendlane.com>
2018-11-26 07:40:02 -0800

    Update view to add comments for blog posts.

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -48,57 +48,61 @@
             </div>
             <div class="p-t-lg">
                 <p class="subtitle">Comments</p>
-                    @foreach ($blog->comments as $comment)
-                    <article class="media">
-                    <figure class="media-left">
-                            <p class="image is-64x64">
-                              <img src="https://bulma.io/images/placeholders/128x128.png">
-                            </p>
-                          </figure>
-                          <div class="media-content">
-                            <div class="content">
-                                    {{ $comment->created_at->diffForHumans() }}: &nbsp;
-                                    {{ $comment->body  }}
-                            </div>
-                          </div>
-                          @if (Auth::check())
-                            <form action="comment/{{$comment->id}}" method="POST">
-                                {{ csrf_field() }}
-                                {{ method_field('DELETE') }}
-                                
-                                <button class="button is-light" type="submit">Delete</button>
-                            </form>
-                          @endif
-                        </figure>
-                    </article>
-                    @endforeach
+                @foreach ($blog->comments as $comment)
+                <article class="media">
+                <figure class="media-left">
+                    <p class="image is-64x64">
+                        <img src="https://bulma.io/images/placeholders/128x128.png">
+                    </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+                                {{ $comment->created_at->diffForHumans() }}: &nbsp;
+                                {{ $comment->body  }}
+                        </div>
+                    </div>
+                    @if (Auth::check())
+                    <form action="comment/{{$comment->id}}" method="POST">
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE') }}
+                        <button class="button is-light" type="submit">Delete</button>
+                    </form>
+                    @endif
+                    </figure>
+                </article>
+                @endforeach
             </div>
-           
+            <div class="p-t-lg">
+                <form method="post" action="/blog/{{ $blog->id }}/comments">
+                    {{ csrf_field() }}
+                    <article class="media">
+                        <figure class="media-left">
+                            <p class="image is-64x64">
+                                <img src="https://bulma.io/images/placeholders/128x128.png">
+                            </p>
+                        </figure>
+                        <div class="media-content">
+                            <div class="field">
+                                <p class="control">
+                                    <textarea name="body" class="textarea" placeholder="Add a comment..."></textarea>
+                                </p>
+                            </div>
+                            <div class="field">
+                                <p class="control">
+                                    <button type="submit" class="button">Post comment</button>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                @include('partials.errors')
+                </form>
+            </div>
         </div>
     </section>
 
     <div class="container">
 
     </div>
-
-    <section class="comments">
-        <div class="column is-8 is-offset-2">
-            <form method="post" action="/blog/{{ $blog->id }}/comments">
-                {{ csrf_field() }}
-                <div>
-                    <textarea name="body"
-                    cols="30" rows="5"
-                    placeholder="Leave a comment."
-                    required>
-                    </textarea>
-                </div>
-                <div>
-                    <button type="submit">Add Comment</button>
-                </div>
-                @include('partials.errors')
-            </form>
-        </div>
-    </section>
 </div>
 
 
