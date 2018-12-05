a21ecdbb4857a88aaf98a9efe73b23fcf6fb06ab
Jerrik <jerrik+intelmac@sendlane.com>
2018-12-05 11:16:34 -0800

    Add Sendlane Tracking pixel.

---
+++ resources/views/partials/sendlaneTracking.blade.php
@@ -0,0 +1,7 @@
+<script>
+    window._Sendlane = window._Sendlane || [];
+    _Sendlane.push({
+        event_id: '80HagXjLGexDp'
+    });
+    </script>
+<script src="https://sendlane.com/js/eventing.js" async></script>
